800f3753524128b6ee9b46b6802bd45d32da45f9
CarlosEAG <carlos.eag93@gmail.com>
2023-07-17 15:38:07 -0600

    Added start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Flags } from "./components/Flags"
 import { GameContextProvider } from "./components/GameContextProvider"
 import { Smiley } from "./components/Smiley"
@@ -5,16 +6,24 @@ import { Test } from "./components/Test"
 import { Timer } from "./components/Timer"
 
 function App() {
+  const [start, setStart] = useState(false);
   return (
     <>
       <div>
         Super Minesweeper Ultra HD Turbo!
       </div>
       <GameContextProvider>
-        <Flags/>
-        <Test/>
-        <Smiley/>
-        <Timer/>
+        {!start 
+        ? <div onClick={()=>setStart(true)}>
+            Start!
+          </div>
+        :<>
+          <Flags/>
+          <Test/>
+          <Smiley/>
+          <Timer/>
+        </>
+        }
       </GameContextProvider>
     </>
   )
